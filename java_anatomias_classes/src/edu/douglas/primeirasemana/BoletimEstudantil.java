d97bf308fa194238c02f92c055ce907146451b9a
GreekDS <ds423323@gmail.com>
2024-09-28 23:47:11 -0300

    Refatorar a nota do boletim estudantil

---
+++ java_anatomias_classes/src/edu/douglas/primeirasemana/BoletimEstudantil.java
@@ -0,0 +1,17 @@
+package edu.douglas.primeirasemana;
+public class BoletimEstudantil {
+
+    public static void main (String[] args)
+    {
+        // Entrada para a nota do aluno
+        int mediaFinal = 7;
+        if(mediaFinal < 6)
+            System.out.println("REPROVADO");
+    
+        else if(mediaFinal == 6)
+            System.out.println("PROVA MINERVA");
+        
+        else 
+            System.out.println("APROVADO");
+    }
+}
--- java_anatomias_classes/src/edu/douglas/segundasemana/BoletimEstudantil.java
+++ java_anatomias_classes/src/edu/douglas/segundasemana/BoletimEstudantil.java
@@ -1,3 +1,4 @@
+package edu.douglas.segundasemana;
 public class BoletimEstudantil {
 
     public static void main (String[] args)
